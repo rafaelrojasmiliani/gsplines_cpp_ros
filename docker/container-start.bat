282aef94c41f3c80d54f6f56e8d159216517fd63
lab <lab@lab.it>
2022-04-28 19:56:39 +0200

    change docker image

--- docker/container-start.bat
+++ docker/container-start.bat
@@ -2,10 +2,10 @@
 for /F "tokens=1,2,3" %%A in ('netsh interface ip show addresses "vEthernet (Default Switch)" ^| findstr "IP Address"') DO (set xserverip=%%C)
 
 rem START /B C:\"Program Files"\VcXsrv\vcxsrv.exe
-docker pull rafa606/ros_noetic_vim
+docker pull rafa606/vim-ros-noetic-gsplines
 docker run -it ^
         --volume %CD%\..:/workspace/src ^
         --env="DISPLAY=%xserverip%:0.0" ^
         --entrypoint="/bin/bash" ^
         --workdir="/workspace" ^
-        "rafa606/ros_noetic_vim" -c "addgroup --gid 11021 %USERNAME% --force-badname;  adduser --gecos \"\" --disabled-password  --uid 11021 --gid 11021 %USERNAME% --force-badname --home /home/%USERNAME%; install -d -m 0755 -o %USERNAME% -g 11021 /home/%USERNAME%;  usermod -a -G video %USERNAME%; echo %USERNAME% ALL=\(ALL\) NOPASSWD:ALL >> /etc/sudoers; sudo -EHu %USERNAME%  bash"
+        "rafa606/vim-ros-noetic-gsplines" -c "addgroup --gid 11021 %USERNAME% --force-badname;  adduser --gecos \"\" --disabled-password  --uid 11021 --gid 11021 %USERNAME% --force-badname --home /home/%USERNAME%; install -d -m 0755 -o %USERNAME% -g 11021 /home/%USERNAME%;  usermod -a -G video %USERNAME%; echo %USERNAME% ALL=\(ALL\) NOPASSWD:ALL >> /etc/sudoers; sudo -EHu %USERNAME%  bash"
