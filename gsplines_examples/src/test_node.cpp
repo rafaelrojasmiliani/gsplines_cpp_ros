bbf16995a50cdf4e85ebcc101d76a6977074b39c
Rafael Rojas <rojas@imfusion.com>
2024-10-02 16:51:19 +0200

    Fix example

--- gsplines_examples/src/test_node.cpp
+++ gsplines_examples/src/test_node.cpp
@@ -16,7 +16,7 @@ int main(int argc, char **argv) {
 
   Eigen::MatrixXd wp(Eigen::MatrixXd::Random(number_of_wp, codom_dim));
 
-  gsplines::GSpline trj = gsplines::optimization::optimal_sobolev_norm(
+  auto trj = gsplines::optimization::optimal_sobolev_norm(
       wp, gsplines::basis::BasisLegendre(6), {{1.0, 3}}, exec_time);
 
   while (ros::ok()) {
